b21e0572da17742f7e39666ff5e9b7c780f4a736
Evan Varan <evan.varan@gmail.com>
2026-07-04 18:37:42 -0500

    Clerk handshake issue

--- src/start.ts
+++ src/start.ts
@@ -1,4 +1,4 @@
-import { createStart } from "@tanstack/react-start";
+import { createMiddleware, createStart } from "@tanstack/react-start";
 import { clerkMiddleware } from "@clerk/tanstack-react-start/server";
 
 declare const process: {
@@ -9,6 +9,24 @@ const hasClerkServerConfig = Boolean(
   process.env.CLERK_SECRET_KEY && process.env.CLERK_PUBLISHABLE_KEY,
 );
 
+const portalAuthMiddleware = createMiddleware().server(async ({ request, next }) => {
+  const url = new URL(request.url);
+
+  if (url.searchParams.has("__clerk_handshake") && !url.pathname.startsWith("/portal")) {
+    url.pathname = "/portal";
+    throw new Response(null, {
+      status: 307,
+      headers: { Location: url.toString() },
+    });
+  }
+
+  if (!url.pathname.startsWith("/portal")) {
+    return next();
+  }
+
+  return clerkMiddleware()(request, next);
+});
+
 export const startInstance = createStart(() => ({
-  requestMiddleware: hasClerkServerConfig ? [clerkMiddleware()] : [],
+  requestMiddleware: hasClerkServerConfig ? [portalAuthMiddleware] : [],
 }));
