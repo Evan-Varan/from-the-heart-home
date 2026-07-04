a66132435561b5de4c7e4712f34ab376d396c1b9
Evan Varan <evan.varan@gmail.com>
2026-07-04 18:43:21 -0500

    Middleware fix

--- src/start.ts
+++ src/start.ts
@@ -1,32 +1,6 @@
-import { createMiddleware, createStart } from "@tanstack/react-start";
+import { createStart } from "@tanstack/react-start";
 import { clerkMiddleware } from "@clerk/tanstack-react-start/server";
 
-declare const process: {
-  env: Record<string, string | undefined>;
-};
-
-const hasClerkServerConfig = Boolean(
-  process.env.CLERK_SECRET_KEY && process.env.CLERK_PUBLISHABLE_KEY,
-);
-
-const portalAuthMiddleware = createMiddleware().server(async ({ request, next }) => {
-  const url = new URL(request.url);
-
-  if (url.searchParams.has("__clerk_handshake") && !url.pathname.startsWith("/portal")) {
-    url.pathname = "/portal";
-    throw new Response(null, {
-      status: 307,
-      headers: { Location: url.toString() },
-    });
-  }
-
-  if (!url.pathname.startsWith("/portal")) {
-    return next();
-  }
-
-  return clerkMiddleware()(request, next);
-});
-
 export const startInstance = createStart(() => ({
-  requestMiddleware: hasClerkServerConfig ? [portalAuthMiddleware] : [],
+  requestMiddleware: [clerkMiddleware()],
 }));
